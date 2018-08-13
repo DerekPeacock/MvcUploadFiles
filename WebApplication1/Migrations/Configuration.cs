defe92bd57224c52f3f0dc56d38ef809084e0e64
DerekPeacock <derek.peacock@yahoo.com>
2018-08-13 10:55:30 +0100

    Renamed the solution and the project

--- WebApplication1/Migrations/Configuration.cs
+++ WebApplication1/Migrations/Configuration.cs
@@ -1,9 +1,6 @@
 namespace WebApplication1.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
     using WebApplication1.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WebApplication1.Models.StudentDbContext>
